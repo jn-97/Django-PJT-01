9d6b2ba25042589dcb6ea351368d7628068fd63a
jn-97 <kjo5395@gmail.com>
2022-10-10 16:35:56 +0900

    m7 delete 완료

--- reviews/views.py
+++ reviews/views.py
@@ -4,46 +4,47 @@ from .models import Review
 
 # Create your views here.
 
+
 def index(request):
     reviews = Review.objects.all()
-    context = {
-        'reviews': reviews
-    }
-    return render(request, 'reviews/index.html', context)
+    context = {"reviews": reviews}
+    return render(request, "reviews/index.html", context)
+
 
 def create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         review_form = ReviewForm(request.POST)
         if review_form.is_valid():
             review_form.save()
-            return redirect('reviews:index')
+            return redirect("reviews:index")
     else:
         review_form = ReviewForm()
-    context = {
-        'review_form': review_form
-    }
-    return render(request, 'reviews/create.html', context)
+    context = {"review_form": review_form}
+    return render(request, "reviews/create.html", context)
+
 
 def detail(request, pk):
     review = Review.objects.get(pk=pk)
-    context = {
-        'review': review
-    }
-    return render(request, 'reviews/detail.html', context)
+    context = {"review": review}
+    return render(request, "reviews/detail.html", context)
+
 
 def update(request, pk):
     review = Review.objects.get(pk=pk)
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         review_form = ReviewForm(request.POST, instance=review)
         if review_form.is_valid():
             review_form.save()
-            return redirect('reviews:detail', review.pk)
+            return redirect("reviews:detail", review.pk)
     else:
         review_form = ReviewForm(instance=review)
-    context = {
-        'review_form': review_form
-    }
-    return render(request, 'reviews/update.html', context)
+    context = {"review_form": review_form}
+    return render(request, "reviews/update.html", context)
 
 
+def delete(request, pk):
+    review = Review.objects.get(pk=pk)
+    review.delete()
+
+    return redirect("reviews:index")
